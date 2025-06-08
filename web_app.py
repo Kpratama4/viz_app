3a3da864519e817b52e6817ef27c7872acb46bf8
Kpratama4 <165520143+Kpratama4@users.noreply.github.com>
2025-06-08 11:52:21 +0200

    Add files via upload

---
+++ web_app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+st.title("Hello World!")
+st.header ("Welcome to MyGenAI App")
+
+check_on = st.checkbox("Check me!")
+if check_on:
+    st.write("checkbox is checked!")
+
+name = st.text_input("Enter your name:")
+if name:
+    st.write(f"Hello {name}!")
+iris_data.csv)
+
+import pandas as pd
+
+import pandas as pd
+data = pd.read_csv("iris_data.csv")
+
+import plotly.express as px
+fig = px.scatter(data, x="SepalLengthCm", y="SepalWidthCm", color="Species")
+st.plotly_chart(fig, use_container_width=True)
